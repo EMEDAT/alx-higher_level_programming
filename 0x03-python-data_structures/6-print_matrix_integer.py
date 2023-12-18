d73662f3361d11a2fab1343f1793c4fa3467be13
EMEDAT <emedattitus24@gmail.com>
2023-12-18 00:56:21 -0800

    i am back

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
-x_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end="")
-            if j != (len(matrix[i]) - 1):
-                print(" ", end="")
-
-        print("")
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
